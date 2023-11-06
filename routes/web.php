e261fffdb93a967229db68e15660564367be3f5a
Foo Bar <124519337+Ulduz23@users.noreply.github.com>
2023-11-06 10:24:36 +0400

    web route change

--- routes/web.php
+++ routes/web.php
@@ -4,69 +4,68 @@ use App\Http\Controllers\adminController;
 use Illuminate\Support\Facades\Route;
 
 
-use App\Http\Controllers\brandcontroller;
-use App\Http\Controllers\clientController;
-use App\Http\Controllers\GoogleLogin;
-use App\Http\Controllers\ordersController;
-use App\Http\Controllers\productController;
-use App\Http\Controllers\profileController;
-use App\Http\Controllers\staffController;
-use App\Http\Controllers\staffdocController;
-use App\Http\Controllers\staticController;
-use App\Http\Controllers\userController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\StaffdocController;
+use App\Http\Controllers\StaticController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 
 Route::group(['middleware'=>'islogin'],function(){
     //BRANDS 
 
-    Route::post('/brands',[brandcontroller::class,'insert'])->name('insert'); //insert 
-    Route::get('/select',[brandcontroller::class,'select'])->name('select'); // select
-    Route::get('brandsil/{id}',[brandcontroller::class,'sil'])->name('sil'); //sil he
-    Route::get('branddelete/{id}',[brandcontroller::class,'delete'])->name('delete'); // sil
-    Route::get('brandedit/{id}',[brandcontroller::class,'edit'])->name('edit');
-    Route::post('brandupdate',[brandcontroller::class,'update'])->name('update');
-    Route::post('/search',[brandcontroller::class,'search'])->name('search'); // select
+    Route::post('/brands',[BrandController::class,'insert'])->name('insert'); //insert 
+    Route::get('/select',[BrandController::class,'select'])->name('select'); // select
+    Route::get('brandsil/{id}',[BrandController::class,'sil'])->name('sil'); //sil he
+    Route::get('branddelete/{id}',[BrandController::class,'delete'])->name('delete'); // sil
+    Route::get('brandedit/{id}',[BrandController::class,'edit'])->name('edit');
+    Route::post('brandupdate',[BrandController::class,'update'])->name('update');
+    Route::post('/search',[BrandController::class,'search'])->name('search'); // select
     //----------------------------------------CLIENTS--------------------------------------------------//
 
-    Route::post('/clients',[clientController::class,'insert'])->name('clinsert'); //insert 
-    Route::get('/cselect',[clientController::class,'select'])->name('cselect'); // select
-    Route::get('clientsil/{id}',[clientController::class,'sil'])->name('csil'); //sil he
-    Route::get('clientsdel/{id}',[clientController::class,'delete'])->name('cdelete'); // sil
-    Route::get('clientsedit/{id}',[clientController::class,'edit'])->name('cedit');
-    Route::post('clientsupdate',[clientController::class,'update'])->name('cupdate');
-    Route::post('/csearch',[clientController::class,'search'])->name('csearch'); // select
+    Route::post('/clients',[ClientController::class,'insert'])->name('clinsert'); //insert 
+    Route::get('/cselect',[ClientController::class,'select'])->name('cselect'); // select
+    Route::get('clientsil/{id}',[ClientController::class,'sil'])->name('csil'); //sil he
+    Route::get('clientsdel/{id}',[ClientController::class,'delete'])->name('cdelete'); // sil
+    Route::get('clientsedit/{id}',[ClientController::class,'edit'])->name('cedit');
+    Route::post('clientsupdate',[ClientController::class,'update'])->name('cupdate');
+    Route::post('/csearch',[ClientController::class,'search'])->name('csearch'); // select
 
     //------------------------------------------PRODUCT--------------------------------------------------------//
 
-    Route::post('/pinsert',[productController::class,'insert'])->name('pinsert'); //insert 
-    Route::get('/pselect',[productController::class,'select'])->name('pselect'); // select
-    Route::get('psil/{id}',[productController::class,'sil'])->name('psil'); //sil he
-    Route::get('pdelete/{id}',[productController::class,'delete'])->name('pdelete'); // sil
-    Route::get('pedit/{id}',[productController::class,'edit'])->name('pedit');
-    Route::post('pupdate',[productController::class,'update'])->name('pupdate');
-    Route::post('/psearch',[productController::class,'search'])->name('psearch'); // select
+    Route::post('/pinsert',[ProductController::class,'insert'])->name('pinsert'); //insert 
+    Route::get('/pselect',[ProductController::class,'select'])->name('pselect'); // select
+    Route::get('psil/{id}',[ProductController::class,'sil'])->name('psil'); //sil he
+    Route::get('pdelete/{id}',[ProductController::class,'delete'])->name('pdelete'); // sil
+    Route::get('pedit/{id}',[ProductController::class,'edit'])->name('pedit');
+    Route::post('pupdate',[ProductController::class,'update'])->name('pupdate');
+    Route::post('/psearch',[ProductController::class,'search'])->name('psearch'); // select
 
 
 
     //------------------------------------------ORDERS-----------------------------------------------------------//
 
-    Route::post('/',[ordersController::class,'insert'])->name('orinsert'); //insert 
-    Route::get('/',[ordersController::class,'select'])->name('orselect'); // select
-    Route::get('orsil/{id}',[ordersController::class,'sil'])->name('orsil'); //sil he
-    Route::get('ordelete/{id}',[ordersController::class,'delete'])->name('ordelete'); // sil
-    Route::get('oredit/{id}',[ordersController::class,'edit'])->name('oredit');
-    Route::post('orupdate',[ordersController::class,'update'])->name('orupdate');
-    Route::get('tesdiq/{id}',[ordersController::class,'tesdiq'])->name('tesdiq');
-    Route::get('legv/{id}',[ordersController::class,'legv'])->name('legv');
-    Route::post('/osearch',[ordersController::class,'search'])->name('osearch'); // select
+    Route::post('/',[OrdersController::class,'insert'])->name('orinsert'); //insert 
+    Route::get('/',[OrdersController::class,'select'])->name('orselect'); // select
+    Route::get('orsil/{id}',[OrdersController::class,'sil'])->name('orsil'); //sil he
+    Route::get('ordelete/{id}',[OrdersController::class,'delete'])->name('ordelete'); // sil
+    Route::get('oredit/{id}',[OrdersController::class,'edit'])->name('oredit');
+    Route::post('orupdate',[OrdersController::class,'update'])->name('orupdate');
+    Route::get('tesdiq/{id}',[OrdersController::class,'tesdiq'])->name('tesdiq');
+    Route::get('legv/{id}',[OrdersController::class,'legv'])->name('legv');
+    Route::post('/osearch',[OrdersController::class,'search'])->name('osearch'); // select
 
     
     //------------------------------------------PROFILE-------------------------------//
 
-    Route::get('/myprofile',[profileController::class,'index'])->name('myprofile');
-    Route::post('/profile',[profileController::class,'profile'])->name('profile');
-    Route::get('/profilsil/{id}',[profileController::class,'sil'])->name('profilsil'); //sil he
-    Route::post('/profildel',[profileController::class,'delete'])->name('profildelete'); // sil
+    Route::get('/myprofile',[ProfileController::class,'index'])->name('myprofile');
+    Route::post('/profile',[ProfileController::class,'profile'])->name('profile');
+    Route::get('/profilsil/{id}',[ProfileController::class,'sil'])->name('profilsil'); //sil he
+    Route::post('/profildel',[ProfileController::class,'delete'])->name('profildelete'); // sil
         //-----------------------------------------ADMIN----------------------------------//
 
     Route::post('/admininsert',[adminController::class,'insert'])->name('adinsert');
@@ -80,29 +79,29 @@ Route::group(['middleware'=>'islogin'],function(){
 
     //----------------------------------STAFF----------------------------------//
 
-    Route::post('/isci',[staffController::class,'insert'])->name('isinsert'); //insert 
-    Route::get('/iscisel',[staffController::class,'select'])->name('isselect'); // select
-    Route::get('iscisil/{id}',[staffController::class,'sil'])->name('issil'); //sil he
-    Route::get('iscidelete/{id}',[staffController::class,'delete'])->name('isdelete'); // sil
-    Route::get('isciedit/{id}',[staffController::class,'edit'])->name('isedit');
-    Route::post('isciupdate',[staffController::class,'update'])->name('isupdate');
+    Route::post('/isci',[StaffController::class,'insert'])->name('isinsert'); //insert 
+    Route::get('/iscisel',[StaffController::class,'select'])->name('isselect'); // select
+    Route::get('iscisil/{id}',[StaffController::class,'sil'])->name('issil'); //sil he
+    Route::get('iscidelete/{id}',[StaffController::class,'delete'])->name('isdelete'); // sil
+    Route::get('isciedit/{id}',[StaffController::class,'edit'])->name('isedit');
+    Route::post('isciupdate',[StaffController::class,'update'])->name('isupdate');
 
 
     //----------------------------------STAFFDOC----------------------------------//
 
-    Route::post('/doc',[staffdocController::class,'insert'])->name('docinsert'); //insert 
-    Route::get('/docsel/{id}',[staffdocController::class,'select'])->name('docselect'); // select
-    Route::get('docsil/{id}',[staffdocController::class,'sil'])->name('docsil'); //sil he
-    Route::get('docdelete/{id}',[staffdocController::class,'delete'])->name('docdelete'); // sil
-    Route::get('docedit/{id}',[staffdocController::class,'edit'])->name('docedit');
-    Route::post('docupdate',[staffdocController::class,'update'])->name('docupdate');
+    Route::post('/doc',[StaffdocController::class,'insert'])->name('docinsert'); //insert 
+    Route::get('/docsel/{id}',[StaffdocController::class,'select'])->name('docselect'); // select
+    Route::get('docsil/{id}',[StaffdocController::class,'sil'])->name('docsil'); //sil he
+    Route::get('docdelete/{id}',[StaffdocController::class,'delete'])->name('docdelete'); // sil
+    Route::get('docedit/{id}',[StaffdocController::class,'edit'])->name('docedit');
+    Route::post('docupdate',[StaffdocController::class,'update'])->name('docupdate');
 
-    Route::get('/static',[staticController::class,'hesab'])->name('hesabla');
+    Route::get('/static',[StaticController::class,'hesab'])->name('hesabla');
 
 
     //--------------------------------------LOGOOUT-----------------------------------------//
 
-    Route::get('/logout',  [userController::class, 'logout'])->name('logout');
+    Route::get('/logout',  [UserController::class, 'logout'])->name('logout');
 
 
 
@@ -115,7 +114,7 @@ Route::group(['middleware'=>'notlogin'],function(){
 
     //------------------------------------------LOGIN--------------------------------------//
 
-Route::post('/login',[userController::class,'login'])->name('login');
+Route::post('/login',[UserController::class,'login'])->name('login');
 
 Route::get('/login', function(){
     return view('login');
@@ -124,10 +123,10 @@ Route::get('/login', function(){
 
 //----------------------------REGISTER--------------------------------------------//
 
-Route::post('/qeydiyyat',[userController::class,'register'])->name('register');
+Route::post('/qeydiyyat',[UserController::class,'register'])->name('register');
 
-Route::get('/test',[userController::class,'test'])->name('test');
-Route::get('/user-verification/{verification}',[userController::class,'user_verification'])->name('user_verification');
+Route::get('/test',[UserController::class,'test'])->name('test');
+Route::get('/user-verification/{verification}',[UserController::class,'user_verification'])->name('user_verification');
 
 
 
@@ -136,11 +135,5 @@ Route::get('/qeydiyyat', function(){
 })->name('qeydiyyat');
 
 
-Route::get('google',function(){
-    return view('googleAuth');
-    });
 
-Route::get('auth/google', [GoogleLogin::class,'redirectToGoogle']);
-
-Route::get('auth/google/callback', [GoogleLogin::class,'handleGoogleCallback']);
 });
